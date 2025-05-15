1ca8bc8149e57e31ca8f375d9700e0a1f293897a
ajay-naidu-96 <79222540+ajay-naidu-96@users.noreply.github.com>
2025-05-15 20:00:10 +0000

    Deploying to gh-pages from @ ajay-naidu-96/ajay-naidu-96.github.io@f0b8f861e028ddd5e1bf54c21d3f1053c3f3d78e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -145,35 +145,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%6A%61%79%6E%61%69%64%75%39%36@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/ajay-naidu-96", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-leetcode',
+        title: 'LeetCode',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://leetcode.com/u/ajaynaidu96/", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://www.linkedin.com/in/ajaynaidu96", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/ajaygopii", "_blank");
         },
       },{
       id: 'light-theme',
