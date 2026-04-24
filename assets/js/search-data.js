c70a56aaea182c3fc7948b33213d88296ba5c121
ajay-naidu-96 <79222540+ajay-naidu-96@users.noreply.github.com>
2026-04-24 01:30:45 +0000

    Deploying to gh-pages from @ ajay-naidu-96/ajay-naidu-96.github.io@2630a59e9109933b21fc5cdcb271a426f43f9c3c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -120,6 +120,9 @@ ninja.data = [{
           section: "News",},{id: "news-paper-accepted-to-iclr-2026-proregen-progressive-residual-generation-under-attribute-correlations",
           title: 'Paper accepted to ICLR 2026: ProReGen: Progressive Residual Generation Under Attribute Correlations.',
           description: "",
+          section: "News",},{id: "news-i-officially-hit-225lbs-on-my-bench-one-of-my-long-term-goals",
+          title: 'I officially hit 225lbs on my bench! One of my long term goals....',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
